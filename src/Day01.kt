f35d44a500341ecc9ba43dd711a81b6ed7804309
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2023-12-01 09:01:02 +0100

    AOC 2023: Day1 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,47 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-//    check(testInput.first() == "Foo")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
+    val testInput = readInput("Day01_test")
+
+    check(part1(testInput) == 220)
     part1(input).println()
+
+    check(part2(testInput) == 281)
     part2(input).println()
 }
+
+fun part1(input: List<String>): Int {
+    return input.map { line ->
+        line.filter { it.isDigit() }
+    }.map {
+        it.first().toString() + it.last().toString()
+    }.sumOf {it.toInt()}
+
+}
+
+fun part2(input: List<String>): Int {
+    return input.sumOf{it.firstAndLastNumberAdded()}
+}
+fun String.firstAndLastNumberAdded(): Int {
+    val firstNumberWord = this.findAnyOf(digitWords.keys)
+    val lastNumberWord = this.findLastAnyOf(digitWords.keys)
+    val firstDigit = this.firstOrNull { it.isDigit() }
+    val firstDigitPos = this.indexOfFirst{it == firstDigit} 
+    val lastDigit = this.lastOrNull { it.isDigit() }
+    val lastDigitPos = this.indexOfLast{it == lastDigit}
+    
+    val first = if (firstDigitPos < (firstNumberWord?.first ?: 1000)) firstDigit.toString() else digitWords[firstNumberWord!!.second]!!.toString()
+    val second = if (lastDigitPos > (lastNumberWord?.first ?: -1)) lastDigit.toString() else digitWords[lastNumberWord!!.second]!!.toString()
+    return (first + second).toInt()
+}
+
+val digitWords = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
+)
