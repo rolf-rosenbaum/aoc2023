f329ca88a49bfb60b362c1cefbb4d638c41d2b34
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2023-12-11 16:15:54 +0100

    AOC 2023: Day11 complete

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,17 +1,58 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+private typealias Space = Set<Point>
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+fun main() {
+    fun part1(input: List<String>): Long = input.toGalaxies().expandEmptySpace().sumOfDistances()
+
+    fun part2(input: List<String>): Long = input.toGalaxies().expandEmptySpace(factor = 1_000_000).sumOfDistances()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day11_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day11")
+
+    check(part1(testInput) == 374L)
     part1(input).println()
+
+    check(part2(testInput) == 82000210L)
     part2(input).println()
+}
+
+fun Space.sumOfDistances(): Long {
+    val queue = toMutableList()
+    var distance = 0L
+    while (queue.isNotEmpty()) {
+        val current = queue.removeFirst()
+        distance += queue.fold(0L) { sum, point ->
+            sum + point.distanceTo(current)
+        }
+    }
+    return distance
+}
+
+fun Space.expandEmptySpace(factor: Int = 2): Space {
+    val emptyColumns = (minOf(Point::x)..maxOf(Point::x)).filter { col ->
+        this.none { it.x == col }
+    }
+
+    val emptyRows = (minOf(Point::y)..maxOf(Point::y)).filter { row ->
+        this.none { it.y == row }
+    }
+
+    return this.map { galaxy ->
+        val newX = galaxy.x + (factor - 1) * emptyColumns.count { it < galaxy.x }
+        Point(newX, galaxy.y)
+    }.map { galaxy ->
+        val newY = galaxy.y + (factor - 1) * emptyRows.count { it < galaxy.y }
+        Point(newY, galaxy.x)
+    }.toSet()
+}
+
+fun List<String>.toGalaxies(): Space {
+    return mutableSetOf<Point>().let {
+        this.forEachIndexed { y, line ->
+            line.forEachIndexed { x, c ->
+                if (c == '#') it.add(Point(x, y))
+            }
+        }
+        it
+    }
 }
